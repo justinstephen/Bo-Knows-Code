2a1fde30ef1fd6317bd95ae85623a1ed862779df
Justin Phillips <justnisdead@gmail.com>
2016-10-12 14:30:40 -0700

    Added back predictions code

--- LM Scores.R
+++ LM Scores.R
@@ -1,11 +1,9 @@
 library(GGally)
 library(MASS)
 
-scores <- as.data.frame(read.csv("scores.csv", header = TRUE))
+scores <- as.data.frame(read.csv("scores2.csv", header = TRUE))
 scores <- scores[,2:ncol(scores)]
 
-M <- cor(scores)
-corrplot(M, method="number")
 ggpairs(scores, alpha = 0.4)
 
 initial.fit <- lm(Score~.,scores)
@@ -14,3 +12,10 @@ par(mfrow=c(2,2))
 plot(initial.fit)
 plot(final.fit)
 summary(final.fit)
+
+modified.fit <- lm(Score ~ X4WeekAvg + Max + Min, scores)
+
+test.scores <- scores[c(4, 5, 6)]
+scores$prediction <- predict(modified.fit, newdata = test.scores)
+
+plot(scores$Score, scores$prediction)
