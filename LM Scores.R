a6a6b67c9543d34ad04a70d9258e70122b0f2adb
Justin Phillips <justnisdead@gmail.com>
2016-10-13 15:11:35 -0700

    Update LM Scores.R

--- LM Scores.R
+++ LM Scores.R
@@ -19,3 +19,36 @@ test.scores <- scores[c(4, 5, 6)]
 scores$prediction <- predict(modified.fit, newdata = test.scores)
 
 plot(scores$Score, scores$prediction)
+
+library(caret)
+
+#Reload the dataset
+scores <- as.data.frame(read.csv("scores2.csv", header = TRUE))
+scores <- scores[,2:ncol(scores)]
+
+#Train a new linear model with all features
+caret.fit <- train(Score~., data = scores, method = "lm")
+
+#Plot importance of each feature
+importance <- varImp(caret.fit, scale = FALSE)
+plot(importance)
+
+#Check out some feature selection
+set.seed(10)
+subsets <- c(1:5, 10, 15, 20, 25)
+
+x <- scores[1:ncol(scores)-1]
+y <- scores[ncol(scores)][,1]
+
+ctrl <- rfeControl(functions = lmFuncs,
+                   method = "repeatedcv",
+                   repeats = 5,
+                   verbose = FALSE)
+
+lmProfile <- rfe(x, y,
+                 sizes = subsets,
+                 rfeControl = ctrl)
+
+lmProfile
+
+trellis.par.set(caretTheme())
