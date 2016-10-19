d0f7ba2240fd3faa53ce0ee4d1bf8b7858c86305
Justin Phillips <justnisdead@gmail.com>
2016-10-18 22:16:44 -0700

    Update Records.R

--- Records.R
+++ Records.R
@@ -1,7 +1,11 @@
-library(dplyr)
+library(googlesheets)
 library(reshape2)
+library(dplyr)
 
-scores <- as.data.frame(read.csv("allscores.csv", header = TRUE))
+sheet <- gs_key("18ha8LKIP1GM_m9drhEVTVlS7-8iK62X6xlIzD092MiU")
+scores <- as.data.frame(sheet %>% gs_read(ws = "Scores"))
+
+#scores <- as.data.frame(read.csv("allscores.csv", header = TRUE))
 
 merged.scores <- left_join(scores, scores, by = c("Opponent" = "Team", "Season" = "Season", "Week" = "Week"))
 merged.scores <- na.omit(merged.scores[1:7])
@@ -9,9 +13,9 @@ merged.scores <- na.omit(merged.scores[1:7])
 merged.scores$Win.x <- ifelse(merged.scores$Score.x > merged.scores$Score.y, 1, 0)
 
 opp.record <- merged.scores %>%
-                group_by(Team, Opponent) %>%
-                summarize(Wins = sum(Win.x),
-                          Games = n()) 
+  group_by(Team, Opponent) %>%
+  summarize(Wins = sum(Win.x),
+            Games = n()) 
 
 records <- merged.scores %>%
   group_by(Team, Opponent) %>%
@@ -20,8 +24,8 @@ records <- merged.scores %>%
 record.matrix <- acast(records, Team~Opponent, value.var="Record")
 
 total.record <- merged.scores %>%
-                  group_by(Team) %>%
-                  summarize(Record = paste(sum(Win.x), "-", n()-sum(Win.x)),
-                            Win.Per = paste(formatC(sum(Win.x)/n() * 100), "%"),
-                            Avg.Points.For = formatC(mean(Score.x)),
-                            Avg.Points.Against = formatC(mean(Score.y)))
+  group_by(Team) %>%
+  summarize(Record = paste(sum(Win.x), "-", n()-sum(Win.x)),
+            Win.Per = paste(formatC(sum(Win.x)/n() * 100), "%"),
+            Avg.Points.For = formatC(mean(Score.x)),
+            Avg.Points.Against = formatC(mean(Score.y)))
