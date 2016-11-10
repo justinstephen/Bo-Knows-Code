3c4ff86ee1daa84d58f37021950ed9b55adb6bf8
Justin Phillips <justnisdead@gmail.com>
2016-11-10 13:50:36 -0800

    Update ui.R

--- apps/matchup/ui.R
+++ apps/matchup/ui.R
@@ -1,23 +1,29 @@
 library(shiny)
 
 shinyUI(fluidPage(
-
+  
   # Application title
-  titlePanel(paste("Week")),
-
+  titlePanel("Week", textOutput("week")),
+  
   # Sidebar
   fluidRow(
     column(3, 
            uiOutput("var")),
     column(9,
-        fluidRow(
-          column(6,
-                 textOutput("teamA"),
-                 textOutput("teamAper")),
-          column(6,
-                 textOutput("teamB"),
-                 textOutput("teamBper")),
-        plotOutput("plot"))
+           fluidRow(
+             column(6, align="center",
+                    span(
+                      h3(textOutput("teamA")),
+                      h1(textOutput("teamAper")),
+                      style = "color:#ef8a62")),
+             column(6, align="center",
+                    span(
+                      h3(textOutput("teamB")),
+                      h1(textOutput("teamBper")),
+                      style = "color:#67a9cf")),
+             plotOutput("l.plot"),
+             br(), br(), br(), br(), br(), br(),
+             plotOutput("d.plot"))
     )
   )
 ))
